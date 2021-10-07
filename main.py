e84a4dd0d50c7913e277b352b47f1b91a8b1c4c6
Evgeny <evgenyshmidt@gmail.com>
2021-10-07 22:59:33 +0300

    +

--- main.py
+++ main.py
@@ -24,15 +24,39 @@ class Widget(QtWidgets.QWidget):
         self.r.setFont(QtGui.QFont("Hack Nerd Font", 14))
         self.r.setWordWrapMode(True)
         self.r.setAcceptRichText(False)
+        self.cmp = QtWidgets.QPushButton('=')
+        self.cmp.clicked.connect(self.on_cmp)
         
         self.l_r = QtWidgets.QPushButton('->')
         self.r_l = QtWidgets.QPushButton('<-')
-        self.lt.addWidget(self.l, 0, 0, 3, 1)
+        self.lt.addWidget(self.l, 0, 0, 4, 1)
         self.lt.addWidget(self.l_r, 0, 1)
         self.lt.addWidget(self.r_l, 1, 1)
-        self.lt.addWidget(self.r, 0, 2, 3, 1)
+        self.lt.addWidget(self.cmp, 3, 1)
+        self.lt.addWidget(self.r, 0, 2, 4, 1)
         self.l_r.clicked.connect(self.to_right)
         self.r_l.clicked.connect(self.to_left)
+
+    def on_cmp(self):
+        lstr = self.l.toPlainText()
+        matches = re.finditer(regex, lstr, re.MULTILINE)
+        ldata={}
+        for matchNum, match in enumerate(matches, start=0):
+            ldata[match.group(1)] = match.group(2)
+
+        rstr = self.r.toPlainText()
+        matches = re.finditer(regex, rstr, re.MULTILINE)
+        rdata={}
+        for matchNum, match in enumerate(matches, start=0):
+            rdata[match.group(1)] = match.group(2)
+        diff = {}
+        for (i,j) in rdata.items():
+            if i not in ldata:
+                diff[i]=j
+        for (i,j) in ldata.items():
+            if i not in rdata:
+                diff[i]=j
+        self.r.setText("\n".join(['{}: "{}"'.format(i, j) for (i,j) in diff.items()]))
 
 
     def to_left(self):
