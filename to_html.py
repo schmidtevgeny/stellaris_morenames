adf5357f1a0e83650c293fb2be5a174523bad415
Evgeny <evgenyshmidt@gmail.com>
2022-05-06 19:41:03 +0300

    +

--- to_html.py
+++ to_html.py
@@ -5,6 +5,7 @@ import os.path
 import re
 from bs4 import BeautifulSoup
 import html
+import click
 
 '''
 Собрать языковые файлы со всех доступных модов в один пакет
@@ -33,9 +34,9 @@ def has_rus(s):
 
 
 # giga_achievement_05_title
-def load_file(base, config, locpath, exclisive=False):
+def load_file(base, config, locpath, substitute, exclisive = False):
     print(base)
-    f = open(base, encoding='utf-8')
+    f = open(base, encoding = 'utf-8')
     data = "\n".join([x for x in f.read().split('\n') if x.strip() != '' and x.strip()[0] != '#'])
     f.close()
     base = os.path.relpath(base, locpath)
@@ -49,12 +50,22 @@ def load_file(base, config, locpath, exclisive=False):
     matches = re.finditer(regex, data, re.MULTILINE)
     rdata = []
 
-    for matchNum, match in enumerate(matches, start=1):
+    for matchNum, match in enumerate(matches, start = 1):
         try:
+            key = match.group(1)
+            val = match.group(2).replace('%', '%%')
+            regex_sub = r"\$(.*?)\$"
+            matches_sub = re.finditer(regex_sub, val, re.MULTILINE)
+
+            for matchNum_sub, match_sub in enumerate(matches_sub, start = 1):
+                subst = match_sub.group(1)
+                if not subst in substitute:
+                    substitute.append(subst)
+
             if not config.has_option(base, match.group(1)):
-                config.set(base, match.group(1), match.group(2).replace('%', '%%'))
+                config.set(base, key, val)
             else:
-                new = match.group(2).replace('%', '%%')
+                new = val
                 old = config.get(base, match.group(1))
                 if has_rus(new):
                     config.set(base, match.group(1), new)
@@ -64,36 +75,12 @@ def load_file(base, config, locpath, exclisive=False):
     pass
 
 
-module_path = "C:/Program Files (x86)/Steam/steamapps/workshop/content/281990/"
-# module_path = "test/"
-modules = [
-    1595876588, 1688887083, 946222466, 1067631798,
-    1890399946, 1121692237, 1311725711, 1333526620,
-    1623423360, 1780481482, 2604778880, 683230077,
-    1587178040, 1630649870, 2458945473, 2484636075,
-    2466607238, 2555609401, 1701916892, 2411818376,
-    1701915595, 1796418794, 1796402967, 1302897684,
-    1419304439, 1489142966, 1313138123, 2509070395,
-    727000451, 1885775216, 2028826064, 2458024521,
-    2293169684, 790903721,
-    2529002857, 2622652746, 2626285356, 2475302050,
-    # loc
-    1487654111, 1375388095, 1670045745, 2486026362,
-    2166824852, 2617298932, 2615894270, 2609978732,
-    2037347735, 1982183037, 1830669482, 2702693226
-]
-print('load predef')
-config_en = configparser.ConfigParser()
-config_en.optionxform = str
-
-
 class locstring:
     def __init__(self, path, value):
         self.path = path
         self.value = value
 
     def replace(self, fr, to):
-        # print(fr, to)
         self.value = self.value.replace(fr, to)
 
     def parse(self):
@@ -113,28 +100,39 @@ class locstring:
         return s.replace('\\n', '<br/>')
 
 
-def process(config: configparser.ConfigParser, path, default={}):
+def process(config: configparser.ConfigParser):
     data = {}
-
+    cnt = 0
     for i in config.sections():
         for j in config[i]:
             data[j] = locstring(i + '/' + j, config[i][j])
+            cnt += 1
+    return data
 
-    for i in default:
-        fr = '$' + i + '$'
-        to = default[i]
-        for j in data:
-            data[j].replace(fr, to)
 
-    for i in data:
-        fr = '$' + i + '$'
-        to = data[i].value
-        for j in data:
-            if i != j:
+def find_subst(data, substitute, default):
+    print('find_subst')
+    for i in substitute:
+        if i in data.keys():
+            default[i] = data[i].value
+        else:
+            lost_subst.append(i)
+
+
+def update_subst(data, default):
+    print('update_subst')
+    with click.progressbar(length = len(default)) as bar:
+        for i in default:
+            fr = '$' + i + '$'
+            to = default[i]
+            for j in data:
                 data[j].replace(fr, to)
+            bar.update(1)
 
+
+def save_data(data, path):
+    print('save_data')
     soup = BeautifulSoup('<html><body></body></html>', 'lxml')
-
     for i in data:
         tag = soup.new_tag('div')
         tag['class'] = data[i].path
@@ -142,58 +140,154 @@ def process(config: configparser.ConfigParser, path, default={}):
         # for it in ss.body:
         tag.append(ss.body.p)
         soup.body.append(tag)
-
-    with open(path, "w", encoding='utf-8') as config_file:
+    with open(path, "w", encoding = 'utf-8') as config_file:
         config_file.write(str(soup.prettify()))
 
+
+def save_data(data, path):
+    print('save_data')
+    soup = BeautifulSoup('<html><body></body></html>', 'lxml')
+    for i in data:
+        tag = soup.new_tag('div')
+        tag['class'] = data[i].path
+        ss = BeautifulSoup(data[i].parse(), 'lxml')
+        # for it in ss.body:
+        tag.append(ss.body.p)
+        soup.body.append(tag)
+    with open(path, "w", encoding = 'utf-8') as config_file:
+        config_file.write(str(soup.prettify()))
+
+
+def save_data_limit(data, path, limit):
+    print('save_data')
+    soup = BeautifulSoup('<html><body></body></html>', 'lxml')
+    num = 1
+    count = 0
+    for i in data:
+        count += 1
+        if count >= limit:
+            with open(path.format(num), "w", encoding = 'utf-8') as config_file:
+                config_file.write(str(soup.prettify()))
+            soup = BeautifulSoup('<html><body></body></html>', 'lxml')
+            num += 1
+            count = 0
+
+        tag = soup.new_tag('div')
+        tag['class'] = data[i].path
+        ss = BeautifulSoup(data[i].parse(), 'lxml')
+        # for it in ss.body:
+        tag.append(ss.body.p)
+        soup.body.append(tag)
+
+    with open(path.format(num), "w", encoding = 'utf-8') as config_file:
+        config_file.write(str(soup.prettify()))
+
+
+module_path = "C:/Program Files (x86)/Steam/steamapps/workshop/content/281990/"
+app_path = "C:/Program Files (x86)/Steam/steamapps/common/Stellaris/localisation/"
+# module_path = "test/"
+modules = [
+    1595876588, 1688887083, 946222466, 1067631798,
+    1890399946, 1121692237, 1311725711, 1333526620,
+    1623423360, 1780481482, 2604778880, 683230077,
+    1587178040, 1630649870, 2458945473, 2484636075,
+    2466607238, 2555609401, 1701916892, 2411818376,
+    1701915595, 1796418794, 1796402967, 1302897684,
+    1419304439, 1489142966, 1313138123, 2509070395,
+    727000451, 1885775216, 2028826064, 2458024521,
+    2293169684, 790903721,
+    2529002857, 2622652746, 2626285356, 2475302050,
+    # loc
+    1487654111, 1375388095, 1670045745, 2486026362,
+    2166824852, 2617298932, 2615894270, 2609978732,
+    2037347735, 1982183037, 1830669482, 2702693226
+    ]
+# print('load predef')
+config_en = configparser.ConfigParser()
+config_en.optionxform = str
+
+config_en_orig = configparser.ConfigParser()
+config_en_orig.optionxform = str
 
 # config = configparser.ConfigParser()
 # config.optionxform = str
 print('scan')
 
-default_en = {'sr_dark_matter': 'dark matter'}
-default_ru = {'sr_dark_matter': 'Темная материя'}
+default_en = {}
+default_ru = {}
+lost_subst = []
+substitute = []
 
 for module in modules:
     locpath = module_path + str(module) + "/localisation/"
     for fn in glob.iglob(locpath + "*" + en):
-        load_file(fn, config_en, locpath)
+        load_file(fn, config_en, locpath, substitute)
 
     for subsection in subsections:
         for fn in glob.iglob(locpath + subsection + "*" + en):
-            load_file(fn, config_en, locpath)
+            load_file(fn, config_en, locpath, substitute)
 
     for fn in glob.iglob(locpath + "english/*" + en):
-        load_file(fn, config_en, locpath)
+        load_file(fn, config_en, locpath, substitute)
 
-process(config_en, 'en-full.html', default_en)
+for fn in glob.iglob(app_path + "english/*.yml"):
+    load_file(fn, config_en_orig, app_path + "english/", substitute)
+
+# print(substitute)
+
+data_en = process(config_en)
+data_en_orig = process(config_en_orig)
+
+find_subst(data_en, substitute, default_en)
+find_subst(data_en_orig, substitute, default_en)
+
+update_subst(data_en, default_en)
+save_data(data_en, 'en-full.html')
 
 config_ru = configparser.ConfigParser()
 config_ru.optionxform = str
+config_ru.read('ru.ini', 'utf-8')
 
+config_ru_orig = configparser.ConfigParser()
+config_ru_orig.optionxform = str
+
+substitute = []
 for module in modules:
     locpath = module_path + str(module) + "/localisation/"
     for fn in glob.iglob(locpath + "*" + ru):
-        load_file(fn, config_ru, locpath)
+        load_file(fn, config_ru, locpath, substitute)
 
     for subsection in subsections:
         for fn in glob.iglob(locpath + subsection + "*" + ru):
-            load_file(fn, config_ru, locpath)
+            load_file(fn, config_ru, locpath, substitute)
 
     for fn in glob.iglob(locpath + "russian/*" + ru):
-        load_file(fn, config_ru, locpath)
+        load_file(fn, config_ru, locpath, substitute)
 
+for fn in glob.iglob(app_path + "russian/*.yml"):
+    load_file(fn, config_ru_orig, app_path + "russian/", substitute)
 
-def remove_old(process, template):
-    for i in process.sections():
-        if not i in template:
-            process.remove_section(i)
-        else:
-            for j in process[i]:
-                if not j in template[i]:
-                    process.remove_option(i, j)
+data_ru = process(config_ru)
+data_ru_orig = process(config_ru_orig)
 
+find_subst(data_ru, substitute, default_ru)
+find_subst(data_ru_orig, substitute, default_ru)
 
-process(config_ru, 'ru.html', default_ru)
-process(config_ru, 'en.html', default_en)
+for key in default_en.keys():
+    if not key in default_ru.keys():
+        default_ru[key] = default_en[key]
 
+update_subst(data_ru, default_ru)
+
+data_en_crop = {}
+data_ru_crop = {}
+print('compare lang')
+for i in data_ru.keys():
+    if i in data_en.keys() and has_rus(data_ru[i].value):
+        data_en_crop[i] = data_en[i]
+        data_ru_crop[i] = data_ru[i]
+
+save_data_limit(data_ru_crop, 'tr-ru-{}.html', 1000)
+save_data_limit(data_en_crop, 'tr-en-{}.html', 1000)
+
+print("done")
