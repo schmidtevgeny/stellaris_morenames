86f0543be25a6fd1bddb7aec786aece626accd5a
Evgeny <evgenyshmidt@gmail.com>
2022-05-10 22:25:02 +0300

    po

--- to_po.py
+++ to_po.py
@@ -79,7 +79,9 @@ def make_maker(storage: Storage):
     f.write('''import os
 import gettext
 
-gettext.install('update', './locale')    
+# gettext.install('messages')    
+lang_translations = gettext.translation('base', localedir='locales', languages=['ru'])
+lang_translations.install()
     
 base = 'loc/'
 if not os.path.exists(base+'russian'):
@@ -94,7 +96,7 @@ if not os.path.exists(base+'events'):
     for i, fdata in storage.data.items():
         of = i + '_l_russian.yml'
         f.write("f = open(base+'" + of + "', 'w', encoding='utf-8')\n")
-
+        f.write('f.write("l_russian:\\n\\n")\n\n')
         for j, l in fdata.items():
             s = l.get()
             f.write('key="{}"\n'.format(j))
@@ -112,48 +114,88 @@ if not os.path.exists(base+'events'):
     pass
 
 
-def make_memory():
+def make_memory(storage_en, storage_ru):
+    po = open('memory.po', 'w', encoding = 'utf-8')
+
+    for i, f_en in storage_en.data.items():
+        if i in storage_ru.data.keys():
+            f_ru = storage_ru.data[i]
+            for j, e in f_en.items():
+                if j in f_ru.keys():
+                    r = f_ru[j]
+                    if r.get() != e.get():
+                        po.write('msgid "{}"\nmsgstr "{}"\n\n\n'.format(e.get().replace('"', '\\"'),
+                                                                        r.get().replace('"', '\\"')))
+    po.close()
     pass
 
 
-# module_path = "test/"
-modules = [
-    1595876588, 1688887083, 946222466, 1067631798,
-    1890399946, 1121692237, 1311725711, 1333526620,
-    1623423360, 1780481482, 2604778880, 683230077,
-    1587178040, 1630649870, 2458945473, 2484636075,
-    2466607238, 2555609401, 1701916892, 2411818376,
-    1701915595, 1796418794, 1796402967, 1302897684,
-    1419304439, 1489142966, 1313138123, 2509070395,
-    727000451, 1885775216, 2028826064, 2458024521,
-    2293169684, 790903721,
-    2529002857, 2622652746, 2626285356, 2475302050,
-    # плагины переводов
-    # 1487654111, 1375388095, 1670045745, 2486026362,
-    # 2166824852, 2617298932, 2615894270, 2609978732,
-    # 2037347735, 1982183037, 1830669482, 2702693226
-    ]
-
+storage = Storage()
 module_path = "C:/Program Files (x86)/Steam/steamapps/workshop/content/281990/"
 app_path = "C:/Program Files (x86)/Steam/steamapps/common/Stellaris/localisation/"
-storage = Storage()
-# load_strings(storage, "C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\281990\\2087871245\\localisation\\english\\fallen_weaponry_l_english.yml", "fallen_weaponry" )
 
-for module in modules:
-    locpath = module_path + str(module) + "/localisation/"
-    for fn in glob.iglob(locpath + "*" + en):
-        fr = os.path.basename(fn).replace('_l_english.yml', '')
-        load_strings(storage, fn, fr)
 
-    for subsection in subsections:
-        for fn in glob.iglob(locpath + subsection + "*" + en):
-            fr = subsection + os.path.basename(fn).replace('_l_english.yml', '')
+def get_plugin_strings(storage: Storage):
+    # module_path = "test/"
+    modules = [
+        1595876588, 1688887083, 946222466, 1067631798,
+        1890399946, 1121692237, 1311725711, 1333526620,
+        1623423360, 1780481482, 2604778880, 683230077,
+        1587178040, 1630649870, 2458945473, 2484636075,
+        2466607238, 2555609401, 1701916892, 2411818376,
+        1701915595, 1796418794, 1796402967, 1302897684,
+        1419304439, 1489142966, 1313138123, 2509070395,
+        727000451, 1885775216, 2028826064, 2458024521,
+        2293169684, 790903721,
+        2529002857, 2622652746, 2626285356, 2475302050,
+        # плагины переводов
+        # 1487654111, 1375388095, 1670045745, 2486026362,
+        # 2166824852, 2617298932, 2615894270, 2609978732,
+        # 2037347735, 1982183037, 1830669482, 2702693226
+        ]
+    for module in modules:
+        locpath = module_path + str(module) + "/localisation/"
+        for fn in glob.iglob(locpath + "*" + en):
+            fr = os.path.basename(fn).replace(en, '')
             load_strings(storage, fn, fr)
 
-    for fn in glob.iglob(locpath + "english/*" + en):
-        fr = 'russian/' + os.path.basename(fn).replace('_l_english.yml', '')
+        for subsection in subsections:
+            for fn in glob.iglob(locpath + subsection + "*" + en):
+                fr = subsection + os.path.basename(fn).replace(en, '')
+                load_strings(storage, fn, fr)
+
+        for fn in glob.iglob(locpath + "english/*" + en):
+            fr = 'russian/' + os.path.basename(fn).replace(en, '')
+            load_strings(storage, fn, fr)
+
+
+def get_original_strings(storage):
+    for fn in glob.iglob(module_path + '*/*/*' + en):
+        fr = os.path.basename(fn).replace(en, '')
+        load_strings(storage, fn, fr)
+    for fn in glob.iglob(app_path + 'english/*' + en):
+        fr = os.path.basename(fn).replace(en, '')
         load_strings(storage, fn, fr)
 
-make_maker(storage)
 
-os.system("update.py")
+def get_russian_strings(storage):
+    for fn in glob.iglob(module_path + '*/*/*' + ru):
+        fr = os.path.basename(fn).replace(ru, '')
+        load_strings(storage, fn, fr)
+    for fn in glob.iglob(app_path + 'russian/*' + ru):
+        fr = os.path.basename(fn).replace(ru, '')
+        load_strings(storage, fn, fr)
+
+
+get_plugin_strings(storage)
+
+storage_en = Storage()
+storage_ru = Storage()
+
+get_original_strings(storage_en)
+get_russian_strings(storage_ru)
+
+make_maker(storage)
+make_memory(storage_en, storage_ru)
+
+#os.system("update.py")
