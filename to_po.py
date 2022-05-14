e766160c572938dbe6780de10ba4cb58c4c685ad
Evgeny <evgenyshmidt@gmail.com>
2022-05-14 21:44:15 +0300

    +

--- to_po.py
+++ to_po.py
@@ -57,13 +57,15 @@ class Storage:
                 s += "\n    " + str(l)
         return s
 
+
 def has_rus(s):
     for ch in s:
         if ch >= 'а' and ch <= 'я':
             return True
     return False
 
-def load_strings(storage: Storage, path, relpath, russian:bool=False):
+
+def load_strings(storage: Storage, path, relpath, russian: bool = False):
     regex = r"\s*(\S*):\d*\s*\"(.*)\""
 
     f = open(path, encoding = 'utf-8')
@@ -79,6 +81,7 @@ def load_strings(storage: Storage, path, relpath, russian:bool=False):
         if not russian or has_rus(val):
             storage.add(LocString(relpath, key, val))
 
+
 # todo: substitution
 def make_maker(storage: Storage):
     f = open("update.py", "w", encoding = "utf-8")
@@ -92,7 +95,7 @@ lang_translations.install()
 def pre(s):
     return s.replace('\n', '\\n')
     
-base = 'loc/'
+base = 'localisation/'
 if not os.path.exists(base+'russian'):
     os.makedirs(base+'russian')
 if not os.path.exists(base+'diplo_phrases'):
@@ -103,16 +106,18 @@ if not os.path.exists(base+'events'):
 ''')
 
     for i, fdata in storage.data.items():
+        of = os.path.basename(i + '_l_russian.yml')
         of = i + '_l_russian.yml'
-        f.write("f = open(base+'" + of + "', 'w', encoding='utf-8')\n")
+
+        f.write("f = open(base+'" + of + "', 'w', encoding='utf-8-sig')\n")
         f.write('f.write("l_russian:\\n\\n")\n\n')
-        for j, l in fdata.items():            
+        for j, l in fdata.items():
             f.write('key="{}"\n'.format(j))
             strings = l.get().replace('\\\\', '\\').replace('\\n', '\n').split('\n')
-            
+
             valitems = []
             for s in strings:
-                if s=='':
+                if s == '':
                     valitems.append("\"\"")
                     print('empty string {}'.format(j))
                 elif s.find('"') < 0:
@@ -121,7 +126,7 @@ if not os.path.exists(base+'events'):
                     valitems.append("_(r'{}')".format(s))
                 else:
                     valitems.append("_('''{}''')".format(s))
-                    
+
             f.write('value="\\\\n".join([{}])\n'.format(', '.join(valitems)))
 
             f.write('f.write(\'{}: \"{}\"\\n\'.format(key, pre(value)))\n')
@@ -139,12 +144,13 @@ def make_memory(storage_en, storage_ru):
             for j, e in f_en.items():
                 if j in f_ru.keys():
                     r = f_ru[j]
-                    if r.get() != e.get() and r.get() != '' and e.get()!='':
-                        po.write('msgid "{}"\nmsgstr "{}"\n\n\n'.format(e.get().replace('"', '\\"').replace('\n', ' ').strip(),
-                                                                        r.get().replace('"', '\\"').replace('\n', ' ').strip()))
+                    if r.get() != e.get() and r.get() != '' and e.get() != '':
+                        po.write('msgid "{}"\nmsgstr "{}"\n\n\n'.format(
+                            e.get().replace('"', '\\"').replace('\n', ' ').strip(),
+                            r.get().replace('"', '\\"').replace('\n', ' ').strip()))
                         el = e.get().replace('"', '\\"').replace('\\\\', '\\n').replace('\\n', '\n').strip().split('\n')
                         rl = r.get().replace('"', '\\"').replace('\\\\', '\\n').replace('\\n', '\n').strip().split('\n')
-                        if len(el)==len(rl):
+                        if len(el) == len(rl):
                             for i in range(len(el)):
                                 po.write('msgid "{}"\nmsgstr "{}"\n\n\n'.format(el[i], rl[i]))
     po.close()
@@ -155,20 +161,25 @@ storage = Storage()
 module_path = "C:/Program Files (x86)/Steam/steamapps/workshop/content/281990/"
 app_path = "C:/Program Files (x86)/Steam/steamapps/common/Stellaris/localisation/"
 
-
+# todo: localisation_synced
 def get_plugin_strings(storage: Storage):
     # module_path = "test/"
     modules = [
-        1595876588, 1688887083, 946222466, 1067631798,
-        1890399946, 1121692237, 1311725711, 1333526620,
-        1623423360, 1780481482, 2604778880, 683230077,
-        1587178040, 1630649870, 2458945473, 2484636075,
-        2466607238, 2555609401, 1701916892, 2411818376,
-        1701915595, 1796418794, 1796402967, 1302897684,
-        1419304439, 1489142966, 1313138123, 2509070395,
-        727000451, 1885775216, 2028826064, 2458024521,
-        2293169684, 790903721,
-        2529002857, 2622652746, 2626285356, 2475302050,
+        # base
+        790903721, 2028826064, 1885775216, 1313138123, 1419304439, 1796418794, 1701916892, 2484636075, 683230077,
+        1121692237, 2509070395, 1630649870, 2466607238, 2608299476, 1890399946, 1623423360, 1333526620, 1311725711,
+        2293169684, 1067631798, 946222466, 1688887083, 1419304439,
+        # full
+        # 1595876588, 1688887083, 946222466, 1067631798,
+        # 1890399946, 1121692237, 1311725711, 1333526620,
+        # 1623423360, 1780481482, 2604778880, 683230077,
+        # 1587178040, 1630649870, 2458945473, 2484636075,
+        # 2466607238, 2555609401, 1701916892, 2411818376,
+        # 1701915595, 1796418794, 1796402967, 1302897684,
+        # 1419304439, 1489142966, 1313138123, 2509070395,
+        # 727000451, 1885775216, 2028826064, 2458024521,
+        # 2293169684, 790903721,
+        # 2529002857, 2622652746, 2626285356, 2475302050,
         # плагины переводов
         # 1487654111, 1375388095, 1670045745, 2486026362,
         # 2166824852, 2617298932, 2615894270, 2609978732,
@@ -234,4 +245,4 @@ get_russian_strings(storage_ru)
 make_maker(storage)
 make_memory(storage_en, storage_ru)
 
-#os.system("update.py")
+# os.system("update.py")
